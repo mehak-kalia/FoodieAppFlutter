c17caa4d67559a71c72cb16473224aa0cccc77ed
mehak-kalia <mehakkalia77@gmail.com>
2021-08-25 16:01:07 +0530

    Foodie APP using Flutter and Dart

--- lib/pages/cart-page.dart
+++ lib/pages/cart-page.dart
@@ -22,70 +22,73 @@ class _CartPageState extends State<CartPage> {
   }
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: fetchcartDishes(),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
+    return Scaffold(appBar: AppBar(
+        title: Text(Util.APP_NAME),),
+      body: StreamBuilder(
+        stream: fetchcartDishes(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
 
-        if(snapshot.hasError){
-          return Center(
-            child: Text("SOMETHING WENT WRONG", style: TextStyle(color: Colors.red),),
-          );
-        }
+          if(snapshot.hasError){
+            return Center(
+              child: Text("SOMETHING WENT WRONG", style: TextStyle(color: Colors.red),),
+            );
+          }
 
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        return Container(
-          color: Colors.white70,
-          child: ListView(
-              padding: EdgeInsets.all(16),
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return Container(
+            color: Colors.white70,
+            child: ListView(
+                padding: EdgeInsets.all(16),
 
-              children: snapshot.data!.docs.map<Widget>((DocumentSnapshot document){
-                //document.id -> Restaurant Document ID
-                Map<String, dynamic> map = document.data()! as Map<String, dynamic>;
+                children: snapshot.data!.docs.map<Widget>((DocumentSnapshot document){
+                  //document.id -> Restaurant Document ID
+                  Map<String, dynamic> map = document.data()! as Map<String, dynamic>;
 
-                /*List <dynamic> category = map['category'];
-                String categories = "";
-                category.forEach((element) {
-                  categories += element.toString()+", ";
-                });
+                  /*List <dynamic> category = map['category'];
+                  String categories = "";
+                  category.forEach((element) {
+                    categories += element.toString()+", ";
+                  });
 
-                categories = categories.substring(0, categories.length-2);*/
+                  categories = categories.substring(0, categories.length-2);*/
 
-                return Card(
-                  child: Container(
-                    padding: EdgeInsets.all(16.0),
+                  return Card(
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
 
-                    child: Column(
+                      child: Column(
 
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(map['name'], style: TextStyle(fontSize: 22),),
-                        Row(
-                            children:[
-                              Text("Price \u20b9"+map['price'].toString(), style: TextStyle(color: Colors.black54, fontSize: 18),),
-                              Spacer(),
-                              Container(
-                                padding: EdgeInsets.all(6.0),
-                                child: Text(map['quantity'].toString(), style: TextStyle(color: Colors.white),),
-                                decoration: BoxDecoration(
-                                    color: Colors.green,
-                                    borderRadius: BorderRadius.circular(8.0)
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(map['name'], style: TextStyle(fontSize: 22),),
+                          Row(
+                              children:[
+                                Text("Price \u20b9"+map['price'].toString(), style: TextStyle(color: Colors.black54, fontSize: 18),),
+                                Spacer(),
+                                Container(
+                                  padding: EdgeInsets.all(6.0),
+                                  child: Text(map['quantity'].toString(), style: TextStyle(color: Colors.white),),
+                                  decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(8.0)
+                                  ),
                                 ),
-                              ),
-                            ]
-                        ),
-                      ],
+                              ]
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }).toList()
-          ),
-        );
+                  );
+                }).toList()
+            ),
+          );
 
-      },
+        },
+      ),
     );
 
   }
